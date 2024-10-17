89543fc69a758f6f392ee068c13cad25511af6bf
Marcos Santana Viana <107136037+msvvianna@users.noreply.github.com>
2024-10-17 19:43:12 -0300

    Delete flaskr directory

--- flaskr/static/assets/js/charts.js
+++
@@ -1,9 +0,0 @@
-function charts(data) {
-    data.forEach((ghp, index) => {
-        const div = document.createElement('div')
-        div.id = `graph-${index}`
-        document.getElementById('graphs').appendChild(div)
-        let chart = new ApexCharts(document.getElementById(`graph-${index}`), ghp);
-        chart.render();
-    })
-}
